ab6161d7ddf02cfd03da261503403a7eddb84b7f
Haalrubaye <hassanaalrubaye@hotmail.com>
2022-12-11 23:54:24 +0400

    Initial commit

---
+++ src/main/java/net/haalrubaye/meteorite/Addon.java
@@ -0,0 +1,43 @@
+package net.haalrubaye.meteorite;
+
+import net.haalrubaye.meteorite.commands.CommandExample;
+import net.haalrubaye.meteorite.hud.HudExample;
+import net.haalrubaye.meteorite.modules.ModuleExample;
+import com.mojang.logging.LogUtils;
+import meteordevelopment.meteorclient.addons.MeteorAddon;
+import meteordevelopment.meteorclient.systems.commands.Commands;
+import meteordevelopment.meteorclient.systems.hud.Hud;
+import meteordevelopment.meteorclient.systems.hud.HudGroup;
+import meteordevelopment.meteorclient.systems.modules.Category;
+import meteordevelopment.meteorclient.systems.modules.Modules;
+import org.slf4j.Logger;
+
+public class Addon extends MeteorAddon {
+    public static final Logger LOG = LogUtils.getLogger();
+    public static final Category CATEGORY = new Category("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+
+    @Override
+    public void onInitialize() {
+        LOG.info("Initializing Meteor Addon Template");
+
+        // Modules
+        Modules.get().add(new ModuleExample());
+
+        // Commands
+        Commands.get().add(new CommandExample());
+
+        // HUD
+        Hud.get().register(HudExample.INFO);
+    }
+
+    @Override
+    public void onRegisterCategories() {
+        Modules.registerCategory(CATEGORY);
+    }
+
+    @Override
+    public String getPackage() {
+        return "com.example.addon";
+    }
+}
---
+++ src/main/java/net/haalrubaye/meteorite/modules/ModuleExample.java
@@ -0,0 +1,10 @@
+package net.haalrubaye.meteorite.modules;
+
+import net.haalrubaye.meteorite.Addon;
+import meteordevelopment.meteorclient.systems.modules.Module;
+
+public class ModuleExample extends Module {
+    public ModuleExample() {
+        super(Addon.CATEGORY, "example", "An example module in a custom category.");
+    }
+}
